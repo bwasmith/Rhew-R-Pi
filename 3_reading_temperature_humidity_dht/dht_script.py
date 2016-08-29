87b882579370c2659ff0feffc992ec79dd1e1890
bwasmith <bsmithpolo@gmail.com>
2016-08-29 11:25:50 -0700

    added while loop with delay to dht_script

--- 3_reading_temperature_humidity_dht/dht_script.py
+++ 3_reading_temperature_humidity_dht/dht_script.py
@@ -1,8 +1,14 @@
 import Adafruit_DHT
+from time import sleep
 
+DELAY_INTERVAL = 5
 dht_sensor = Adafruit_DHT.DHT22
 dht_pin = 4
 
-humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
 
-print "Humidity: {:0.1f}%, Temperature: {:0.2f}*C".format(humidity, temperature)
+while True:
+  humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
+
+  print "Humidity: {:0.1f}%, Temperature: {:0.2f}*C".format(humidity, temperature)
+
+  sleep(DELAY_INTERVAL)
