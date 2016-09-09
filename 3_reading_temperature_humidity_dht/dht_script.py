45d39b973bf18922f3b3820dfae76a76342b5f16
bwasmith <bsmithpolo@gmail.com>
2016-09-09 10:49:13 -0700

    updated CONSTANTs to caps

--- 3_reading_temperature_humidity_dht/dht_script.py
+++ 3_reading_temperature_humidity_dht/dht_script.py
@@ -3,11 +3,11 @@ from time import sleep
 
 DELAY_INTERVAL = 5
 dht_sensor = Adafruit_DHT.DHT22
-dht_pin = 4
+DHT_PIN = 4
 
 
 while True:
-  humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
+  humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, DHT_PIN)
 
   print "Humidity: {:0.1f}%, Temperature: {:0.2f}*C".format(humidity, temperature)
 
--- 3_reading_temperature_humidity_dht/simple_dht_script.py
+++ 3_reading_temperature_humidity_dht/simple_dht_script.py
@@ -1,8 +1,8 @@
 import Adafruit_DHT
 
 dht_sensor = Adafruit_DHT.DHT22
-dht_pin = 4
+DHT_PIN = 4
 
-humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
+humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, DHT_PIN)
 
 print "Humidity: {:0.1f}%, Temperature: {:0.2f}*C".format(humidity, temperature)
